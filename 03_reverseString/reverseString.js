0ef80dad306f5fb61ef1a2f18a97ab21c8bb7e35
MarkKnowsNothing <marknguyen.knows.nothing@gmail.com>
2023-01-21 21:16:06 +0700

    Pass exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splittedWord = string.split("");
+    let reverseSplittedWord = splittedWord.reverse()
+    let reversedString = reverseSplittedWord.join("")
+    return reversedString
 };
 
 // Do not edit below this line
