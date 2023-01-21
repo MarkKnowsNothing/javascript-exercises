42977802d9c4c98059fad7922e4b7dcc85a2dfff
MarkKnowsNothing <marknguyen.knows.nothing@gmail.com>
2023-01-21 21:01:50 +0700

    Pass exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function repeatString(string, num) {
+    let newString = string;
+    
+    for (let i = 1; i < num; i++) {
+        newString = newString + string;
+    };
 
+    if (num === 0) {
+        return ""
+    } else if (num < 0) {
+        return "ERROR"
+    } else {
+        return newString
+    }
 };
 
 // Do not edit below this line
