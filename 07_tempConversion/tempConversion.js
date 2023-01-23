b1a1314ea51cbf862d2dd706baa9299b0585f91d
MarkKnowsNothing <marknguyen.knows.nothing@gmail.com>
2023-01-23 21:05:30 +0700

    Pass exercise07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FInput) {
+  let CResult = (FInput - 32) * (5 / 9)
+  let result = Math.round(CResult * 10) / 10
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CInput) {
+  let FResult = (CInput * (9 / 5) + 32)
+  let result = Math.round(FResult * 10) / 10
+  return result
 };
 
 // Do not edit below this line
